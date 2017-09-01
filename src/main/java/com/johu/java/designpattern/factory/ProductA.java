9cc486a089648b6dd5f5ee2c3fc512a241d9eaf4
johu <ywnjohu@gmail.com>
2017-09-01 16:02:02 +0800

    add 设计模式package

---
+++ src/main/java/com/johu/java/designpattern/factory/ProductA.java
@@ -0,0 +1,10 @@
+package com.johu.java.designpattern.factory;
+
+/**
+ * Created by johu on 17-8-25.
+ */
+public class ProductA implements Product {
+    public ProductA(){
+        System.out.println("productA");
+    }
+}
